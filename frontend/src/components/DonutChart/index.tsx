db1834d2792194e0c03fc799c3e96312067f49e3
phanetcrz <phanet@gmail.com>
2021-05-07 21:51:13 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,28 +1,30 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
+type ChartData = {
+    series: number[]; //-- Array de number
+    labels: string[]; //-- Array de string
+}
+
 const DonutChart = () => {
 
-    type ChartData = {
-        series: number[]; //-- Array de number
-        labels: string[]; //-- Array de string
-    }    
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // FORMA ERRADA
-    let chartData : ChartData = { labels: [], series: []}; //let é para criação de variavel         
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`) //--a requisição é realizada de forma assincrona, ela vai se chamada, a sua aplicação continua rodando, e quando voltar a resposta ai sim executa alguma coisa no caso a promice
+            .then(response => { // após a resposta da api executa a promice atrás do then quando foi executado com sucesso
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`) //--a requisição é realizada de forma assincrona, ela vai se chamada, a sua aplicação continua rodando, e quando voltar a resposta ai sim executa alguma coisa no caso a promice
-        .then( response =>{ // após a resposta da api executa a promice atrás do then quando foi executado com sucesso
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName); 
-            const mySeries = data.map(x => x.sum);
-            
-            chartData = { labels: myLabels, series: mySeries};
-            console.log(chartData);            
-        }); 
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    },[]);
+
+    //-- forma que é montada
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
