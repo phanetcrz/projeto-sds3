d37fa6c3df238d37e20eb193080725f3b228cc07
phanetcrz <phanet@gmail.com>
2021-05-07 21:31:48 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,11 +1,32 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
 
 const DonutChart = () => {
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    type ChartData = {
+        series: number[]; //-- Array de number
+        labels: string[]; //-- Array de string
+    }    
+
+    // FORMA ERRADA
+    let chartData : ChartData = { labels: [], series: []}; //let é para criação de variavel         
+
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/sales/amount-by-seller`) //--a requisição é realizada de forma assincrona, ela vai se chamada, a sua aplicação continua rodando, e quando voltar a resposta ai sim executa alguma coisa no caso a promice
+        .then( response =>{ // após a resposta da api executa a promice atrás do then quando foi executado com sucesso
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName); 
+            const mySeries = data.map(x => x.sum);
+            
+            chartData = { labels: myLabels, series: mySeries};
+            console.log(chartData);            
+        }); 
+    // const mockData = {
+    //     series: [477138, 499928, 444867, 220426, 473088],
+    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    // }
 
     const options = {
         legend: {
@@ -15,8 +36,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
